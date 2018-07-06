dfbc2ab7f4f110695ebacd58ea523e11eb473259
Maxime Burlat <maximeburlat@gmail.com>
2018-07-05 23:44:16 -0500

    got a simple connection to online graphql to work

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,46 @@
-'use strict';
+"use strict";
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+const axios = require("axios");
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "kettlecat" is now active!');
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "kettlecat" is now active!');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with  registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand("extension.sayHello", () => {
+    // The code you place here will be executed every time your command is executed
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
-
+    axios({
+      url: "https://kettlecat-graphql.herokuapp.com/graphql",
+      method: "get",
+      data: {
+        query: `
+            query{
+              chakiboo(id: "5b35956ff455f5c193fdaf8d"){
+                title
+              }
+            }
+          `
+      }
+    })
+      .then(result => {
         // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
+        console.log(result.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
 
-    context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {
-}
+export function deactivate() {}
